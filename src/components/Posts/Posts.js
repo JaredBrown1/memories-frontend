df34b8a173e3cc596567a42a35110eae1a4f15d2
Jared Brown <jaredbrown1@hotmail.com>
2021-06-18 18:52:39 -0700

    memory card styles implemented

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,19 +1,30 @@
 import React from "react";
-import Post from "./Post/Post";
+import { Grid, CircularProgress } from "@material-ui/core";
 import { useSelector } from "react-redux";
+
 import useStyles from "./styles";
+import Post from "./Post/Post";
 
 const Posts = () => {
 	const posts = useSelector((state) => state.posts);
 	const classes = useStyles();
 
 	console.log(posts);
-	return (
-		<>
-			<h1 className={classes.Posts}>POSTS</h1>
-			<Post />
-			<Post />
-		</>
+	return !posts.length ? (
+		<CircularProgress />
+	) : (
+		<Grid
+			className={classes.container}
+			container
+			alignItems="stretch"
+			spacing={3}
+		>
+			{posts.map((post) => (
+				<Grid key={post._id} item xs={12} sm={6}>
+					<Post post={post} />
+				</Grid>
+			))}
+		</Grid>
 	);
 };
 
